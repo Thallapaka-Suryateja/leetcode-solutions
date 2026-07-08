c251317a9762322ec40571517cfdf8c51d9f3704
Thallapaka Suryateja <tsuryateja902@gmail.com>
2026-07-08 21:39:23 +0530

    Time: 16 ms (30.42%), Space: 12.5 MB (17.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x<0:
+        if x<0 or (x%10==0 and x!=0):
             return False
-        n=x
-        l=[]
-        while n>0: 
-            l.append(n%10)
-            n//=10
-        for i in range(len(l)//2):
-            if l[i]!=l[-1-i]:
-                return False
-        return True
+        reversed_half=0
+        while x > reversed_half:
+            reversed_half = reversed_half*10+x%10
+            x//=10
+        return x == reversed_half or x == reversed_half//10
