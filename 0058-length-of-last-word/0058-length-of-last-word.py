09de2a4744fda0dbeb010321d0fe970e20d4df71
Thallapaka Suryateja <tsuryateja902@gmail.com>
2026-07-12 22:36:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (78.69%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,5 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        l=s.strip().split(" ")
-        return len(l[-1])
+        i=len(s)-1
+        while i>=0 and s[i]==" ":
+            i-=1
+        length=0
+        while i>=0 and s[i]!=" ":
+            length+=1
+            i-=1
+        return length
