b1c7a83e15b930cf0120e3fb9b016c1696079b21
Thallapaka Suryateja <tsuryateja902@gmail.com>
2026-07-14 23:00:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.23%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,21 +4,10 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry=0
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]!=9 and i==len(digits)-1:
-                digits[i]=digits[i]+1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
                 return digits
-            elif digits[i]==9 and i!=0:
-                carry=1
-                digits[i]=0
-            elif digits[i]!=9 and i!=len(digits)-1:
-                digits[i]=digits[i]+carry
-                return digits
-            elif digits[i]==9 and i==0:
-                digits[i]=1
-                digits.append(0)
-                return digits
-        
-        
-        return digits
+            digits[i] = 0
+
+        return [1] + digits
