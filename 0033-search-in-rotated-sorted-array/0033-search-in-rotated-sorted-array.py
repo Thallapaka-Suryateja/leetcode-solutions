a44e7b3f7963b751f6db67eaabe88e7d1a18bd07
Thallapaka Suryateja <tsuryateja902@gmail.com>
2026-08-19 22:39:47 +0530

    Time: 0 ms (100%), Space: 12.6 MB (45.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,29 +5,29 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        def binsearch(low,high,nums,target):
-            while(low<=high):
-                mid = low + (high-low)//2
-                
-                if nums[mid] == target:
-                    return mid
-                if nums[mid]<target:
-                    low=mid+1
-                else:
-                    high=mid-1
-            return -1
+        low = 0
+        high = len(nums) - 1
 
-        i=1
-        while i < len(nums):
-            if nums[i] < nums[i-1]:
-                break
-            i+=1
-        
-        idx1=binsearch(0,i-1,nums,target)
-        idx2=binsearch(i,len(nums)-1,nums,target)
-        if idx1 == -1 and idx2 == -1:
-            return -1
-        elif idx1 == -1:
-            return idx2
-        else:
-            return idx1
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if nums[mid] == target:
+                return mid
+
+
+            if nums[low] <= nums[mid]:
+
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+    
+            else:
+
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
