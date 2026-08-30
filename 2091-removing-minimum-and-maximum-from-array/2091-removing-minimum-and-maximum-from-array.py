d2b65faebad5056ea85503c89cf25eef6571d6b7
Thallapaka Suryateja <tsuryateja902@gmail.com>
2026-08-30 23:52:29 +0530

    Time: 52 ms (16.33%), Space: 21.9 MB (12.25%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -6,12 +6,16 @@ class Solution(object):
         """
         n = len(nums)
 
-        min_idx = nums.index(min(nums))
-        max_idx = nums.index(max(nums))
+        min_idx = max_idx = 0
+
+        for i in range(n):
+            if nums[i] < nums[min_idx]:
+                min_idx = i
+            if nums[i] > nums[max_idx]:
+                max_idx = i
 
         left = max(min_idx, max_idx) + 1
         right = n - min(min_idx, max_idx)
-
         both = min(min_idx, max_idx) + 1 + n - max(min_idx, max_idx)
 
         return min(left, right, both)
